51ced5b0d98df9c4ab7dd4a1f46802ea7df96deb
alexandru.ai <andra2699@yahoo.com>
2024-06-02 16:57:49 +0300

    fixed feateredProperties list

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,15 +1,42 @@
-import useFetch from "../../hooks/useFetch";
+import { useState, useEffect } from 'react';
 import "./featuredProperties.css";
 
 const FeaturedProperties = () => {
-  const { data, loading, error } = useFetch("/hotels?featured=true&limit=4");
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/hotels?limit=4");
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log("Data ", data);
 
   return (
     <div className="fp">
+      {console.log("Loading Data ", data)}
       {loading ? (
         "Loading"
+      ) : error ? (
+        <div>Error: {error.message}</div>
       ) : (
         <>
+          {console.log("Mapped Data ", data)}
           {data.map((item) => (
             <div className="fpItem" key={item._id}>
               <img
@@ -20,10 +47,12 @@ const FeaturedProperties = () => {
               <span className="fpName">{item.name}</span>
               <span className="fpCity">{item.city}</span>
               <span className="fpPrice">Starting from ${item.cheapestPrice}</span>
-              {item.rating && <div className="fpRating">
-                <button>{item.rating}</button>
-                <span>Excellent</span>
-              </div>}
+              {item.rating && (
+                <div className="fpRating">
+                  <button>{item.rating}</button>
+                  <span>Excellent</span>
+                </div>
+              )}
             </div>
           ))}
         </>
